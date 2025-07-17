502d099d43093cbaf818a9e2b7be3a51e15239fd
zengdehui <523228155a@gmail.com>
2025-07-17 22:28:21 +0800

    数据生成代码

---
+++ data_simulation/scp_generate.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+
+def create_scp(folder_paths, scp_path):
+    with open(scp_path, 'w', encoding='utf-8') as f:
+        for folder in folder_paths:
+            for root, _, files in os.walk(folder):
+                for file in files:
+                    if file.endswith('.wav'):
+                        full_path = os.path.abspath(os.path.join(root, file))
+                        f.write(full_path + '\n')
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate speech/noise/rir for training.")
+    parser.add_argument("--speech_dir",default=[])
+    parser.add_argument("--noise_dir",default=[])
+    parser.add_argument("--rir_dir",default=[])
+
+    args = parser.parse_args()
+    
+    create_scp(args.speech_dir,'./scp/speech.scp')
+    create_scp(args.noise_dir,'./scp/noise.scp')
+    create_scp(args.rir_dir,'./scp/rir.scp')
+
+         
+
+if __name__ == '__main__':
+    main()   
